c3a46c6457cb7fef30b47cb79fa5e6269a01329e
Russell Branca <chewbranca@apache.org>
2018-04-21 14:31:13 -0700

    Update top level main.lua environment

--- main.lua
+++ main.lua
@@ -1,11 +1,16 @@
 -- Thanks to: https://gitlab.com/technomancy/fennel-tiled-tech-demo/blob/master/main.lua
 -- A shim to bridge fennel and love2d
 fennel = require("fennel")
+lume = require("lume")
 
+table[("insert")](package[("loaders")], fennel[("searcher")])
 pps = function(x)
    return require("serpent").block(x, {maxlevel=8,maxnum=64,
                                        nocode=true,comment=false})
 end
+ppsl = function(x) return require("serpent").line(x) end
 pp = function(x) print(pps(x)) end
+ppl = function(x) print(ppsl(x)) end
+log = function(...) print(string.format(...)) end
 
 fennel.dofile("main.fnl")
